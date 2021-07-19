b3c0c75a09ebe36833ded2ce399ebb26aac68daa
unknown <jaewoo010207@gmail.com>
2021-07-19 23:40:25 +0900

    #4.1 Rendering the Movies

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
   state = {
-    isLoading : true
+    isLoading: true,
+    movies : []
   };
-  
+
   getMovies = async () => {
-    const movies = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
+    console.log(movies);
+    this.setState({movies, isLoading : false})
   }
   componentDidMount() {
     this.getMovies();
   }
 
   render() {
-    const {isLoading} = this.state;
+    const { isLoading, movies } = this.state;
     return (
-      <div>{isLoading ? "Loading" : "We are ready"}</div>
+      <div>{isLoading ? "Loading" : movies.map(movie => {
+        console.log(movie);
+        return (
+          <Movie
+          key = {movie.id}
+          id = {movie.id}
+          year = {movie.year}
+          title = {movie.title}
+          summary = {movie.summary}
+          poster = {movie.medium_cover_image}
+          />
+        ) 
+      })}</div> //map은 항상 뭔가를 리턴해줘야됨 
     );
   }
 }
